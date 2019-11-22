afedd8b73da0f66f5c0bdfeb764f1acf75af2c11
Josi Reis-West <josialexa85@gmail.com>
2019-11-22 12:05:24 -0600

    Completed step 6

--- src/components/Topics/Palindrome.js
+++ src/components/Topics/Palindrome.js
@@ -1,9 +1,47 @@
 import React, {Component} from 'react'
 
 class Palindrome extends Component {
+    constructor() {
+        super()
+        this.state = {
+            input: '',
+            isPalindrome: ''
+        }
+    }
+
+    handleChange(input) {
+        this.setState({
+            input: input
+        })
+    }
+
+    handleClick() {
+        let {input, isPalindrome} = this.state
+
+        isPalindrome = input == input.split('').reverse().join('')
+
+        this.setState({
+            isPalindrome: isPalindrome
+        })
+    }
+    
     render() {
         return (
-            <p>Palindrome</p>
+            <div
+            className="puzzleBox filterStringPB">
+            <h4>Palindrome</h4>
+            <input
+                className="inputLine"
+                value={this.state.input}
+                onChange={e => this.handleChange(e.target.value)}></input>
+            <button
+                className="confirmationButton"
+                onClick={() => this.handleClick()}>Check</button>
+            <span
+                className="resultsBox">
+                {`Palindrome: ${this.state.isPalindrome}`}
+            </span>
+        </div>
         )
     }
 }
--- src/components/Topics/Sum.js
+++ src/components/Topics/Sum.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react'
 
 class Sum extends Component {
+
     render() {
         return (
             <p>Sum</p>
