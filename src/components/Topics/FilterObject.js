0b6be31155f24d169db71ab531a5f439881abd44
Josi Reis-West <josialexa85@gmail.com>
2019-11-22 11:22:01 -0600

    Completed step 4

--- src/components/Topics/FilterObject.js
+++ src/components/Topics/FilterObject.js
@@ -1,9 +1,67 @@
 import React, {Component} from 'react'
 
 class FilterObject extends Component {
+    constructor() {
+        super()
+        this.state = {
+            unfiltered: [
+                {
+                  name: 'Jimmy Joe',
+                  title: 'Hack0r',
+                  age: 12,
+                },
+                {
+                  name: 'Jeremy Schrader',
+                  age: 24,
+                  hairColor: 'brown'
+                },
+                {
+                  name: 'Carly Armstrong',
+                  title: 'CEO',
+                }
+            ],
+            input: "",
+            filtered: []
+        }
+    }
+
+    handleChange(input) {
+        this.setState({
+            input: input
+        })
+    }
+
+    handleClick() {
+        let {input, unfiltered, filtered} = this.state
+
+        filtered = unfiltered.filter(v => !(v[input]))
+
+        this.setState({
+            filtered: filtered
+        })
+    }
+
     render() {
         return (
-            <p>FilterObject</p>
+            <div
+                className="puzzleBox filterObjectPB">
+                <h4>Filter Object</h4>
+                <span
+                    className="puzzleText">
+                        {`Original: ${JSON.stringify(this.state.unfiltered)}`}
+                    </span>
+                <input
+                    className="inputLine"
+                    onChange={(e) => this.handleChange(e.target.value)}
+                    value={this.state.input}></input>
+                <button
+                    className="confirmationButton"
+                    onClick={() => this.handleClick()}>Filter</button>
+                <span
+                    className="resultsBox filterObjectRB">
+                        {`Filtered: ${JSON.stringify(this.state.filtered)}`}
+                    </span>
+            </div>
         )
     }
 }
