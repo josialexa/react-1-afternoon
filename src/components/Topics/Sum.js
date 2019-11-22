c713076c81914c9790d7bd162f208e5246b6483f
Josi Reis-West <josialexa85@gmail.com>
2019-11-22 12:31:44 -0600

    Completed step 7

--- src/components/Topics/Sum.js
+++ src/components/Topics/Sum.js
@@ -1,10 +1,57 @@
 import React, {Component} from 'react'
 
 class Sum extends Component {
+    constructor() {
+        super()
+        this.state = {
+            left: 0,
+            right: 0,
+            total: null
+        }
+    }
+
+    handleChangeLeft(input) {
+        this.setState({
+            left: +input
+        })
+    }
+
+    handleChangeRight(input) {
+        this.setState({
+            right: +input
+        })
+    }
+
+    handleClick() {
+        let {left, right, total} = this.state
+
+        total = left + right
+
+        this.setState({
+            total: total
+        })
+    }
 
     render() {
         return (
-            <p>Sum</p>
+            <div
+                className="puzzleBox sumPB">
+                <h4>Sum</h4>
+                <input
+                    className="inputLine"
+                    value={this.state.left == 0 ? '' : this.state.left}
+                    onChange={e => this.handleChangeLeft(e.target.value)}></input>
+                <input
+                    className="inputLine"
+                    value={this.state.right == 0 ? '' : this.state.right}
+                    onChange={e => this.handleChangeRight(e.target.value)}></input>
+                <button
+                    className="confirmationButton"
+                    onClick={() => this.handleClick()}>Add</button>
+                <span className="resultsBox">
+                    {`Sum: ${this.state.total == null ? '' : this.state.total}`}
+                </span>
+            </div>
         )
     }
 }
